42462f8884869e35aed8d0a0c25567188a9591a8
Profe Chris Jara <cjjarav@gmail.com>
2024-08-28 09:36:32 -0500

    completa

--- main.py
+++ main.py
@@ -17,20 +17,33 @@ resultado = restar(a, b)
 print("El resultado de la resta de", a, "y", b, "es:", resultado)
 
 
-# def multiplicar(a, b):
-#     # aqui se realiza la multiplicacion de dos numeros
+def multiplicar(a, b):
 
-# def dividir(a, b):
-#     # aqui se realiza la division de dos numeros
+    # aqui se realiza la multiplicacion de dos numeros
+    return a * b
+a = 5
+b = 3
 
-# def main():
-#     num1 = float(input("Ingrese el primer número: "))
-#     num2 = float(input("Ingrese el segundo número: "))
+resultado = multiplicar(a, b)
+print("El resultado de la multiplicación de", a, "y", b, "es:", resultado)
 
-#     print("Suma:", sumar(num1, num2))
-#     print("Resta:", restar(num1, num2))
-#     print("Multiplicación:", multiplicar(num1, num2))
-#     print("División:", dividir(num1, num2))
+def dividir(a, b):
+    # aqui se realiza la division de dos numeros
+    return a / b
+a = 10
+b = 2
 
-# if __name__ == "__main__":
-#     main()
+resultado = dividir(a, b)
+print("El resultado de la dicivión de", a, "entre", b, "es:", resultado)
+
+def main():
+    num1 = float(input("Ingrese el primer número: "))
+    num2 = float(input("Ingrese el segundo número: "))
+
+    print("Suma:", sumar(num1, num2))
+    print("Resta:", restar(num1, num2))
+    print("Multiplicación:", multiplicar(num1, num2))
+    print("División:", dividir(num1, num2))
+
+if __name__ == "__main__":
+    main()
