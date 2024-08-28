d6131491152cb549de040ec961f2d4111a033b91
Profe Chris Jara <cjjarav@gmail.com>
2024-08-28 09:41:38 -0500

    complete

--- main.py
+++ main.py
@@ -1,40 +1,27 @@
 def sumar(a, b):
     # aqui se realiza la suma de dos numeros
     return a + b
-a = 5
-b = 3
 
 resultado = sumar(a, b)
-print("El resultado de la suma de", a, "y", b, "es:", resultado)
 
 def restar(a, b):
         # aqui se realiza la resta de dos numeros
     return a - b
-a = 5
-b = 3
 
 resultado = restar(a, b)
-print("El resultado de la resta de", a, "y", b, "es:", resultado)
-
 
 def multiplicar(a, b):
 
     # aqui se realiza la multiplicacion de dos numeros
     return a * b
-a = 5
-b = 3
 
 resultado = multiplicar(a, b)
-print("El resultado de la multiplicación de", a, "y", b, "es:", resultado)
 
 def dividir(a, b):
     # aqui se realiza la division de dos numeros
     return a / b
-a = 10
-b = 2
 
 resultado = dividir(a, b)
-print("El resultado de la dicivión de", a, "entre", b, "es:", resultado)
 
 def main():
     num1 = float(input("Ingrese el primer número: "))
